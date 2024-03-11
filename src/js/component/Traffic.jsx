09e146553a22c0cade63b01a52f66a0ea220171f
Carla <152617155+Cl1493@users.noreply.github.com>
2024-03-11 20:24:37 +0000

    logica del componente traffic

--- src/js/component/Traffic.jsx
+++ src/js/component/Traffic.jsx
@@ -1,33 +1,40 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import "./Traffic.css";
+import "../../styles/index.css";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Traffic = () => {
 	const [color,setColor] = useState("red")
 
 	const handleClick = () => {
-	useEffect(() => {
-		setColor ("yellow");
-		setColor ("green");
-	},[color])	
-};
-const handleClick1 = () => {
-	setColor ("green");
+		const colour = ["red", "yellow", "green"];
+		if (color=="red") {
+			setColor("yellow");
+			console.log(color);
+		} else if (color=="yellow") {
+			setColor("green");
+			console.log(color);
+		} else if (color=="green") {
+			setColor("red");
+			console.log(color);
+		}	
 };
 
 	return (
 		<><div className="text-center container">
-			<div className="red">{color}
+			<div className="red">
 			</div>
 			<div className="yellow">
 			</div>
 			<div className="green">
 			</div>
-		</div><button onClick={handleClick, handleClick1}></button></>
+		</div><button onClick={()=> {
+			handleClick();
+			}}>Holi holi</button></>
 	);
 };
 
